ae5903ae76fc5e51d97994b6b5fe12e70a7be31c
king6ava9e <acheampongkingsley2003@gmail.com>
2026-02-16 11:50:50 +0000

    updat

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import tailwindcss from "@tailwindcss/vite";
 export default defineConfig({
   plugins: [tailwindcss()],
   base: "./",
-  base: "/LOCATION-BASED-NEWS-APP-NEAR-ME",
+
   build: {
     outDir: 'dist', 
   }
